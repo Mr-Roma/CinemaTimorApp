b83bc8fbb33861425b1d2d6a87f1ccee1d8c54fc
Romario Marcal <romariomarcal11@gmail.com>
2024-07-05 03:20:59 +0900

    Add the router provider

--- lib/presentation/pages/login_page/login_page.dart
+++ lib/presentation/pages/login_page/login_page.dart
@@ -1,11 +1,5 @@
-import 'package:flix_id/data/dummies/dummy_authentication.dart';
-import 'package:flix_id/data/dummies/dummy_userRepository.dart';
-import 'package:flix_id/data/firebase/firebase_authentication.dart';
-import 'package:flix_id/data/firebase/firebase_user_repository.dart';
-import 'package:flix_id/domain/usecases/login/login.dart';
-import 'package:flix_id/domain/usecases/login/login_params.dart';
-import 'package:flix_id/presentation/pages/main_page/main_page.dart';
-import 'package:flix_id/presentation/providers/usecases/login_provider.dart';
+import 'package:flix_id/presentation/providers/router/router_provider.dart';
+import 'package:flix_id/presentation/providers/user_data/user_data_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,27 +8,24 @@ class LoginPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(userDataProvider, (previous, next) {
+      if (next is AsyncData) {
+        if (next.value != null) {
+          ref.read(routerProvider).goNamed('main');
+        }
+      } else if (next is AsyncError) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(next.error.toString())));
+      }
+    });
     return Scaffold(
         appBar: AppBar(title: Text('Login')),
         body: Center(
           child: ElevatedButton(
               onPressed: () {
-                Login login = ref.watch(loginProvider);
-                login
-                    .call(LoginParams(
-                        email: 'marcal22@gmail.com', password: '123456'))
-                    .then((result) {
-                  if (result.isSuccess) {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                MainPage(user: result.resultValue!)));
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text(result.errorMessage!)));
-                  }
-                });
+                ref
+                    .read(userDataProvider.notifier)
+                    .login(email: 'marcal@gmail.com', password: '123');
               },
               child: Text('Login')),
         ));
--- lib/presentation/pages/main_page/main_page.dart
+++ lib/presentation/pages/main_page/main_page.dart
@@ -1,23 +1,44 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:flix_id/domain/entities/user.dart';
+import 'package:flix_id/presentation/providers/router/router_provider.dart';
+import 'package:flix_id/presentation/providers/user_data/user_data_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MainPage extends StatelessWidget {
-  final User user;
-
+class MainPage extends ConsumerStatefulWidget {
   const MainPage({
     Key? key,
-    required this.user,
   }) : super(key: key);
 
   @override
+  ConsumerState<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends ConsumerState<MainPage> {
+  @override
   Widget build(BuildContext context) {
+    ref.listen(userDataProvider, (previous, next) {
+      if (previous != null && next is AsyncData && next.value == null) {
+        ref.read(routerProvider).goNamed('login');
+      }
+    });
     return Scaffold(
       appBar: AppBar(
         title: Text('MAIN PAGE'),
       ),
       body: Center(
-        child: Text(user.toString()),
+        child: Column(
+          children: [
+            Text(ref.watch(userDataProvider).when(
+                data: (data) => data.toString(),
+                error: (error, stackTrace) => '',
+                loading: () => 'Loading...,')),
+            ElevatedButton(
+                onPressed: () {
+                  ref.read(userDataProvider.notifier).logout();
+                },
+                child: Text('LOGOUT'))
+          ],
+        ),
       ),
     );
   }
